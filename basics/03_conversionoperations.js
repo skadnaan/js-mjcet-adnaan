419f3752ce3999df2da75641591f147029a1a05b
skadnaan <153380976+skadnaan@users.noreply.github.com>
2025-04-26 12:06:26 +0000

    operations

--- basics/03_conversionoperations.js
+++ basics/03_conversionoperations.js
@@ -1,29 +1,50 @@
-let score = "adnaan"
-console.log(typeof(score));
-let valueInNumber = Number(score)
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// let score = "adnaan"
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 // conversion
 // "33" => 33 (gives o/p print as number)
 // "33abc" => NaN(not a number)
 // true => 1; false=0
 
-let isLoggedIn = ""
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true
 // "" => false
 // "adnaan" => true
 
 
-let someNumber = 33
+/*let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
- 
+ */
 // o/p
 
 // 33 => 33
 // typeof => string
+
+// ********************** OPERATIONS ************************ //
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+let str1 = "adnaan"
+let str2 = "ahmed"
+let str = str1 + str2
+console.log(str);
+
+console.log(1+2+"2");
+console.log("1"+2+2);
+console.log("1"+"2"+"2");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+// link to study prefix and postfix
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
