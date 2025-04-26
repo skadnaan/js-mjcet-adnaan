ca67a4917128ae237a16845ac009ed1616e3eac1
skadnaan <153380976+skadnaan@users.noreply.github.com>
2025-04-26 15:28:26 +0000

    strings basics

---
+++ basics/05_strings.js
@@ -0,0 +1,22 @@
+const name = "Adnaan"
+const count = 50
+console.log(`my name is ${name} and count is ${count}`  );
+const gamename = new String(`ady-ahmed`)
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(4));
+// console.log(gamename.indexOf('a'));
+
+// const newString = gamename.substring(0,3)
+// console.log(newString);
+// const dusrastring = gamename.slice(-4,3)
+// console.log(dusrastring);
+const teesrastring = '   awesome    '
+console.log(teesrastring);
+console.log(teesrastring.trim());
+const url = "https://skadnaan.com/adnaan%20ahmed"
+console.log(url.replace('%20' , '-'));
+console.log(url.includes('skady'));
+console.log(gamename.split('-'));
+
