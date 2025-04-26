f83de0001807102e2258eb9701c92af85eb6aa56
skadnaan <153380976+skadnaan@users.noreply.github.com>
2025-04-26 10:35:54 +0000

    conversion

---
+++ basics/03_conversionoperations.js
@@ -0,0 +1,29 @@
+let score = "adnaan"
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// conversion
+// "33" => 33 (gives o/p print as number)
+// "33abc" => NaN(not a number)
+// true => 1; false=0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// "" => false
+// "adnaan" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+ 
+// o/p
+
+// 33 => 33
+// typeof => string
