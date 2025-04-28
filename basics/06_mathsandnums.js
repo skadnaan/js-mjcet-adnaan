784290b06c2114417b3589fd92a68243f8dfd297
skadnaan <153380976+skadnaan@users.noreply.github.com>
2025-04-28 06:36:34 +0000

    maths imp

---
+++ basics/06_mathsandnums.js
@@ -0,0 +1,41 @@
+// const score = 500
+// console.log(score)
+// const bal = new Number(100)
+// console.log(bal)
+
+// console.log(bal.toString());
+// console.log(typeof(bal));
+
+// console.log(bal.toString.length);
+
+// console.log(bal.toFixed(3));
+
+// const secnum = 123.345
+// console.log(secnum.toPrecision(2));
+
+// const num = 1000000
+// console.log(num.toLocaleString('en-IN'));
+
+///////////// ++++++++++++++++++ MATHS ++++++++++++++++++++++//////////////
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-2));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.8));
+// console.log(Math.ceil(4.1));
+// console.log(Math.max(5,4,6,7,7,5,9));
+// console.log(Math.min(5,4,6,7,7,5,9));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log((Math.floor(Math.random()*10) + 1));
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
