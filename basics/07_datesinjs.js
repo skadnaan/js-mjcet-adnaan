4bcab1cb753e199655ac03bef1aea1e23fe56d12
skadnaan <153380976+skadnaan@users.noreply.github.com>
2025-04-28 06:59:52 +0000

    dates

---
+++ basics/07_datesinjs.js
@@ -0,0 +1,19 @@
+// let myDate = new Date ()
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.toDateString());
+
+// console.log(typeof myDate);
+
+// let createdate = new Date(2024,1,25)
+let createdate = new Date("2021-4-24")
+// console.log(createdate.toLocaleDateString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(createdate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
